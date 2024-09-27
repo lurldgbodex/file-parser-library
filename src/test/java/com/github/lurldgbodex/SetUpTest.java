a0ff3d9cd11e5a2e281c4902bfeec04504d9c6e4
Gbodi Segun Clement <42384410+lurldgbodex@users.noreply.github.com>
2024-09-27 08:56:48 +0100

    Setup CI workflow (#15)
    * set up ci pipeline workflow
    
    * add target directory to gitignore
    
    * fix: add checkout repository step
    
    * add basic sample test
    
    * remove java 11 version from workflow
    
    * streamline the trigger for feature branch

---
+++ src/test/java/com/github/lurldgbodex/SetUpTest.java
@@ -0,0 +1,12 @@
+package com.github.lurldgbodex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetUpTest {
+    @Test
+    public void testSetup() {
+        assertTrue(true);
+    }
+}
