de844cf448a7dccacebc4bf66cfbe00134d56d38
Lurldgbodex <professionaleditor6@gmail.com>
2025-03-22 20:29:05 +0100

    chore: change class to record

--- src/test/java/com/github/lurldgbodex/model/User.java
+++ src/test/java/com/github/lurldgbodex/model/User.java
@@ -1,31 +1,11 @@
 package com.github.lurldgbodex.model;
 
 import com.github.lurldgbodex.annotations.FieldMapping;
-import com.github.lurldgbodex.annotations.FileFormat;
 import com.github.lurldgbodex.annotations.ValidationRule;
-import com.github.lurldgbodex.enums.Format;
 
-@FileFormat(format = Format.CSV)
-public class User {
-    @FieldMapping(column = "id")
-    @ValidationRule(required = true, maxLength = 20)
-    private int userId;
-    @FieldMapping(column = "name")
-    @ValidationRule(required = true, minLength = 2)
-    private String name;
-    @FieldMapping(column = "email")
-    @ValidationRule(regex = "^[A-Za-z0-9+_.-]+@(.+)$")
-    private String email;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-}
+public record User(
+        @FieldMapping(column = "name")
+        String name,
+        String email,
+        boolean active
+) {}
