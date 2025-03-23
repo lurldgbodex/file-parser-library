cdaa56abe8578510be42c74d798aa72b9451108c
Lurldgbodex <professionaleditor6@gmail.com>
2025-03-23 19:58:09 +0100

    chore: add map parsing to interface

--- src/main/java/com/github/lurldgbodex/core/Parser.java
+++ src/main/java/com/github/lurldgbodex/core/Parser.java
@@ -6,6 +6,7 @@ import com.github.lurldgbodex.exceptions.ParserException;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 public interface Parser {
     /**
@@ -18,6 +19,17 @@ public interface Parser {
      */
     <T> List<T> parseToObject(File file, Class<T> clazz) throws ParserException;
 
+
+    /**
+     * Parses a file to a list of map
+     *
+     * @param file The file to parse
+     * @return A list of map
+     * @throws ParserException if parsing fails
+     */
+    List<Map<String, Object>> parseToMaps(File file) throws ParserException;
+
+
     /**
      * Converts a file from one format to another format
      *
@@ -27,6 +39,7 @@ public interface Parser {
      * @throws ConverterException if file conversion fails
      */
     void convert(File inputFile, Format outputFormat, File outputFile) throws ConverterException;
+
 
     /**
      * converts a file form one format to another using Java POJO enabling validation
